5ff746ec99663fa509d827e36a804b4cae6a27b5
DQSSSSS <1053181679@qq.com>
2020-12-22 23:44:27 +0800

    hw7 finish

---
+++ 7/Renderer.cpp
@@ -0,0 +1,105 @@
+//
+// Created by goksu on 2/25/20.
+//
+
+#include <fstream>
+#include "Scene.hpp"
+#include "Renderer.hpp"
+#include <thread>
+#include <mutex>
+std::mutex mutex_ins;
+
+inline float deg2rad(const float& deg) { return deg * M_PI / 180.0; }
+
+const float EPSILON = 0.00001;
+
+// The main render function. This where we iterate over all pixels in the image,
+// generate primary rays and cast these rays into the scene. The content of the
+// framebuffer is saved to a file.
+void Renderer::Render(const Scene& scene)
+{
+    std::vector<Vector3f> framebuffer(scene.width * scene.height);
+
+    float scale = tan(deg2rad(scene.fov * 0.5));
+    float imageAspectRatio = scene.width / (float)scene.height;
+    Vector3f eye_pos(278, 273, -800);
+
+    // change the spp value to change sample ammount
+    //int spp = 16; origin
+    int spp = 256;
+    std::cout << "SPP: " << spp << "\n";
+
+    int process = 0;
+    auto deal = [&](int lx,int rx,int ly,int ry) {
+        for (uint32_t j = ly; j <= ry; ++j) {
+            int m = j * scene.width + lx;
+            for (uint32_t i = lx; i <= rx; ++i) {
+                // generate primary ray direction
+                float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+                        imageAspectRatio * scale;
+                float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+
+                Vector3f dir = normalize(Vector3f(-x, y, 1));
+                for (int k = 0; k < spp; k++){
+                    framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+                }
+                m++;
+                process ++;
+            }
+            {
+                std::lock_guard<std::mutex> g1(mutex_ins);
+                UpdateProgress(1.0*process / scene.width / scene.height);
+            }
+        }
+    };
+    int minx = 0, maxx = scene.width-1;
+    int miny = 0, maxy = scene.height-1;
+
+    // std::thread t1(deal, 0, (scene.width-1)/2, 0, scene.height-1);
+    // std::thread t2(deal, (scene.width-1)/2+1, scene.width-1, 0, scene.height-1);
+    // t1.join(); t2.join();
+
+    int bx = 5, by = 5;
+    int nx = (scene.width+bx-1) / bx, ny = (scene.height+by-1) / by; 
+    std::thread th[bx * by];
+    for(int i = 0,id = 0;i < scene.width;i += nx) {
+        for(int j = 0;j < scene.height;j += ny) {
+            th[id] = std::thread(deal, i, std::min(i+nx, scene.width)-1,
+                                       j, std::min(j+ny, scene.height)-1);
+            id ++;
+        }
+    }
+
+    for(int i = 0;i < bx*by;i ++) th[i].join();
+
+
+ /*   int m = 0;
+    for (uint32_t j = 0; j < scene.height; ++j) {
+        for (uint32_t i = 0; i < scene.width; ++i) {
+            // generate primary ray direction
+            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+                      imageAspectRatio * scale;
+            float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+
+            Vector3f dir = normalize(Vector3f(-x, y, 1));
+            for (int k = 0; k < spp; k++){
+                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+            }
+            m++;
+        }
+        UpdateProgress(j / (float)scene.height);
+    }*/
+    UpdateProgress(1.f);
+
+    // save framebuffer to file
+    FILE* fp = fopen("binary.ppm", "wb");
+    (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
+    for (auto i = 0; i < scene.height * scene.width; ++i) {
+        static unsigned char color[3];
+        color[0] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].x), 0.6f));
+        color[1] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].y), 0.6f));
+        color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
+        fwrite(color, 1, 3, fp);
+    }
+    fclose(fp);    
+}
---
+++ 7/Scene.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Göksu Güvendiren on 2019-05-14.
+//
+
+#include "Scene.hpp"
+
+
+void Scene::buildBVH() {
+    printf(" - Generating BVH...\n\n");
+    this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
+}
+
+Intersection Scene::intersect(const Ray &ray) const
+{
+    return this->bvh->Intersect(ray);
+}
+
+void Scene::sampleLight(Intersection &pos, float &pdf) const
+{
+    float emit_area_sum = 0;
+    for (uint32_t k = 0; k < objects.size(); ++k) {
+        if (objects[k]->hasEmit()){
+            emit_area_sum += objects[k]->getArea();
+        }
+    }
+    float p = get_random_float() * emit_area_sum;
+    emit_area_sum = 0;
+    for (uint32_t k = 0; k < objects.size(); ++k) {
+        if (objects[k]->hasEmit()){
+            emit_area_sum += objects[k]->getArea();
+            if (p <= emit_area_sum){
+                objects[k]->Sample(pos, pdf);
+                break;
+            }
+        }
+    }
+}
+
+bool Scene::trace(
+        const Ray &ray,
+        const std::vector<Object*> &objects,
+        float &tNear, uint32_t &index, Object **hitObject)
+{
+    *hitObject = nullptr;
+    for (uint32_t k = 0; k < objects.size(); ++k) {
+        float tNearK = kInfinity;
+        uint32_t indexK;
+        Vector2f uvK;
+        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
+            *hitObject = objects[k];
+            tNear = tNearK;
+            index = indexK;
+        }
+    }
+
+
+    return (*hitObject != nullptr);
+}
+
+
+// Implementation of Path Tracing
+Vector3f Scene::castRay(const Ray &ray, int depth) const
+{
+    // TO DO Implement Path Tracing Algorithm here
+
+    Intersection intersection = intersect(ray);
+    
+    if(!intersection.happened)
+        return Vector3f(0, 0, 0); 
+    if(depth == 0 && intersection.emit.norm()>0) {
+        return intersection.emit;
+    }
+
+    Vector3f& p = intersection.coords;
+    Vector3f wo = normalize(-ray.direction);
+    Vector3f normal = normalize(intersection.normal);
+    Material*& material = intersection.m;
+
+    auto format = [](Vector3f &a) {
+        if(a.x < 0) a.x = 0;
+        if(a.y < 0) a.y = 0;
+        if(a.z < 0) a.z = 0;
+    };
+
+    // direct
+    Vector3f L_direct; 
+    {
+        Intersection inter_dir; 
+        float pdf_dir;
+        sampleLight(inter_dir, pdf_dir);
+
+        Vector3f& x = inter_dir.coords;
+        Vector3f ws = normalize(x - p);
+        Vector3f light_normal = normalize(inter_dir.normal);
+        
+        auto pws = intersect(Ray(p, ws));
+        if(pws.happened && (pws.coords-x).norm() < 1e-2) {
+            L_direct = inter_dir.emit * material->eval(ws, wo, normal) * dotProduct(normal, ws) 
+                * dotProduct(light_normal, -ws) / (dotProduct((x-p), (x-p)) * pdf_dir);
+            format(L_direct);
+        }
+    }
+    // indirect
+    Vector3f L_indirect;
+    {
+        float RR = this->RussianRoulette;
+        if(get_random_float() < RR) {
+            Vector3f wi = normalize(material->sample(wo, normal));
+            L_indirect = castRay(Ray(p, wi), depth+1) 
+                * material->eval(wi, wo, normal) * dotProduct(wi, normal)
+                / (material->pdf(wi, wo, normal) * RR);
+            format(L_indirect);
+        }
+    }
+    return L_direct + L_indirect;
+}
