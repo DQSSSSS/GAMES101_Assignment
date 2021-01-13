ec9f800d97f4dcab117c0f334f518dcc892279b2
DQSSSSS <1053181679@qq.com>
2021-01-14 00:14:02 +0800

    fix z-buffer bug because of changing projection matrix

--- 2/rasterizer.cpp
+++ 2/rasterizer.cpp
@@ -147,7 +147,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
                         float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
                         z_interpolated *= w_reciprocal;
 
-                        if(depColor[get_index(ix, iy)][id].first > z_interpolated) {
+                        if(depColor[get_index(ix, iy)][id].first < z_interpolated) {
                             depColor[get_index(ix, iy)][id].first = z_interpolated;
                             depColor[get_index(ix, iy)][id].second = t.getColor();
                         }
